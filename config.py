5203ade8ed3a960bc7e2d2ec1c08fc3aeee5888b
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-04-17 10:09:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
 
 # Add multiple owner IDs here
 OWNER_IDS = [
-    123456789,   # Owner 1
-    987654321,   # Owner 2
+    5864182070,   # Owner 1
+    8028749711,   # Owner 2
     # Add more owner IDs as needed
 ]
